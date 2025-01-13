949506c293bbb48e984f6a7f41f3921ba9aef565
Ivan Fedorenko <81463720+fedorenkoivan@users.noreply.github.com>
2025-01-13 15:12:26 +0200

    rewrite to avoiding using delete

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -5,16 +5,17 @@
 
 const distinct = (data) => {
   const unique = new Set();
+  const result = [];
+
   for (let i = 0; i < data.length; i++) {
     const element = data[i];
-    if (unique.has(element)) {
-      delete data[i];
-    } else {
+    if (!unique.has(element)) {
       unique.add(element);
+      result.push(element);
     }
   }
 
-  return data.filter((elem) => typeof elem === 'number');
+  return result.filter((elem) => typeof elem === 'number');
 };
 
 module.exports = distinct;
