9665afdbf23d4415d3d18131e8bd3fd3d471ee5e
Ivan Fedorenko <81463720+fedorenkoivan@users.noreply.github.com>
2025-01-13 13:11:04 +0200

    add only one if statement for prefix,suffix checking

--- Tasks/between.js
+++ Tasks/between.js
@@ -5,11 +5,10 @@
 
 const getValueBetween = (string, prefix, suffix) => {
   const prefixIndex = string.indexOf(prefix);
-  if (prefixIndex === -1) return '';
-
   const start = prefixIndex + prefix.length;
-  const suffixIndex = string.indexOf(suffix, start);
-  if (suffixIndex === -1) return '';
+  const suffixIndex = string.indexOf(suffix);
+  if (prefixIndex === -1 || suffixIndex === -1) return '';
+
 
   return string.substring(start, suffixIndex);
 };
