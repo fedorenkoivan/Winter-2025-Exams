fddc5f54e1eec9a1804eb4777c6898a96555e298
Ivan Fedorenko <81463720+fedorenkoivan@users.noreply.github.com>
2025-01-14 11:46:46 +0200

    reduce return statements

--- Tasks/day.js
+++ Tasks/day.js
@@ -8,11 +8,7 @@ const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 const parseDay = (str) => {
   const dayIndex = days.findIndex((day) => str.startsWith(day.toLowerCase()));
 
-  if (dayIndex !== -1) {
-    return dayIndex + 1;
-  } else {
-    return -1;
-  }
+  return dayIndex + 1 || -1;
 };
 
 module.exports = parseDay;
