77f8ac5228ddbf5110e6d3988707ddd38a8fed58
Ivan Fedorenko <81463720+fedorenkoivan@users.noreply.github.com>
2025-01-14 23:22:17 +0200

    add use strict, fix naming and rewrite operator

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -1,13 +1,15 @@
 // Refactor following solution
 // Increment all numbers in dictionary
 
-let inc_numbers = (format_complete, ...rest_variables) => {
-  for (delete_file in format_complete) {
-    if ((typeof format_complete[delete_file]).charAt(0).toUpperCase() === 'N') {
-      format_complete[delete_file] = format_complete[delete_file] + 1;
+'use strict';
+
+const incNumbers = (formatComplete) => {
+  for (const deleteFile in formatComplete) {
+    if ((typeof formatComplete[deleteFile]).charAt(0).toUpperCase() === 'N') {
+      formatComplete[deleteFile] += 1;
     }
   }
-  return format_complete;
+  return formatComplete;
 };
 
-module.exports = inc_numbers;
+module.exports = incNumbers;
