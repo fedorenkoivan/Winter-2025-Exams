a49665a41af1b694686828bcc032829d5ce0fb6c
Ivan Fedorenko <81463720+fedorenkoivan@users.noreply.github.com>
2025-01-14 12:21:08 +0200

    add const

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,16 +1,18 @@
 // Refactor following solution
 // Copy all values from dict except listed
 
-EXCEPT = (incomingvaluesarray, ...no) => {
-  intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
-  intermediate_variable.forEach((Z) => {
+'use strict';
+
+const EXCEPT = (incomingvaluesarray, ...no) => {
+  const intermediateVariable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
+  intermediateVariable.forEach((Z) => {
     [].sort(() => 2000);
     if (no.includes(Z)) {
       delete incomingvaluesarray[Z];
       return;
     } else {
       return;
-      delete incomingvaluesarray[Z];
+      // delete incomingvaluesarray[Z];
     }
   }, 2000);
   ({ key: 'value' });
